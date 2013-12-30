6c398c218feca6f4d8dc66dade070df471d78394
Mark Dessain <mark.dessain@gmail.com>
2013-12-30 01:31:47 +0000

    Adding in stat_with_hash

--- src/file_utils.js
+++ src/file_utils.js
@@ -2,23 +2,41 @@ var fs = require('fs');
 var crypto = require('crypto');
 
 
-hash = function(filePath, callback) {
-    var hash = crypto.createHash('sha1');
-    var fileStream = fs.createReadStream(filePath);
-
+hash = function(path, callback) {
+    var fileStream = fs.createReadStream(path);
     fileStream.on('end', function() {
         hash.end();
         callback(null, hash.read());
     });
-
     fileStream.on('error', function() {
         hash.end();
-        callback('Unable to load file', null);
+        callback('Unable to hash file', null);
     });
 
+    var hash = crypto.createHash('sha1');
     hash.setEncoding('hex');
-    fileStream.pipe(hash);
+
+    fileStream.pipe(hash); 
 }
 
 
+stat_with_hash = function(path, callback) {
+
+    fs.stat(path, function(stat_err, stat_result) {
+        if(stat_err){
+            return callback(stat_err, null);
+        }
+        hash(path, function(hash_err, hash_result) {
+            if(hash_err){
+                return callback(hash_err, null);
+            }
+            stat_result['hash'] = hash_result;
+            callback(null, stat_result)
+        });
+    });
+}
+
+
+
 exports.hash = hash;
+exports.stat_with_hash = stat_with_hash;
--- test/file_utils.js
+++ test/file_utils.js
@@ -1,39 +1,68 @@
-var fs = require('fs')
-var assert = require('assert')
-var should = require('should')
+var fs = require('fs');
+var assert = require('assert');
+var should = require('should');
 
 var file_utils = require('../src/file_utils.js')
 
+describe('utils', function(){
+  describe('hash', function(){  
+    it('should return a hash for an empty file', function(){
+      var filePath = __dirname + '/tmp/empty_file.txt';
+      fs.writeFile(filePath, '', function() {
+        file_utils.hash(filePath, function(err, result) {
+          (err == null).should.be.true
+          result.should.equal('da39a3ee5e6b4b0d3255bfef95601890afd80709');
+          fs.unlink(filePath);
+        });
+      });
+    });
 
-describe('hash', function(){
-  it('should return a hash for an empty file', function(){
-    var filePath = __dirname + '/tmp/empty_file.txt';
-    fs.writeFile(filePath, '', function() {
+    it('should return a hash for a file with content "test"', function(){
+      var filePath = __dirname + '/tmp/test_file.txt';
+      fs.writeFile(filePath, 'test', function() {
+        file_utils.hash(filePath, function(err, result) {
+          (err == null).should.be.true
+          result.should.equal('a94a8fe5ccb19ba61c4c0873d391e987982fbbd3');
+          fs.unlink(filePath);
+        });
+      });
+    });
+
+    it('should return a error for a missing file', function(){
+      var filePath = __dirname + '/tmp/non-file.txt';
       file_utils.hash(filePath, function(err, result) {
-        (err == null).should.be.true
-        result.should.equal('da39a3ee5e6b4b0d3255bfef95601890afd80709');
-        fs.unlink(filePath);
+        err.should.equal('Unable to hash file');
+        (result == null).should.be.true
       });
     });
   });
 
-  it('should return a hash for a file with content "test"', function(){
-    var filePath = __dirname + '/tmp/test_file.txt';
-    fs.writeFile(filePath, 'test', function() {
-      file_utils.hash(filePath, function(err, result) {
-        (err == null).should.be.true
-        result.should.equal('a94a8fe5ccb19ba61c4c0873d391e987982fbbd3');
-        fs.unlink(filePath);
+  describe('stat_with_hash', function(){  
+    it('should return a error if unable to stat file', function(){
+      var filePath = __dirname + '/tmp/non-file.txt';
+      file_utils.stat_with_hash(filePath, function(err, result) {
+        err.code.should.equal('ENOENT');
+        (result == null).should.be.true
       });
     });
-  });
 
-  it('should return an error for a missing file', function(){
-    var filePath = __dirname + '/tmp/non-file.txt';
-    file_utils.hash(filePath, function(err, result) {
-      err.should.equal('Unable to load file');
-      (result == null).should.be.true
+    it('should return a error if unable to hash file', function(){
+      // TODO: Unsure how to do this
+    });
+
+    it('should return a stat and hash for a valid file', function(){
+      var filePath = __dirname + '/tmp/hash-file.txt';
+      fs.writeFile(filePath, 'test', function() {
+        file_utils.stat_with_hash(filePath, function(err, result) {
+          (err == null).should.be.true
+          result.hash.should.equal('a94a8fe5ccb19ba61c4c0873d391e987982fbbd3');
+          result.size.should.equal(4);
+          fs.unlink(filePath);
+        });
+      });
     });
   });
 });
+
+
 
